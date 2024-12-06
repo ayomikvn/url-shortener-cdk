e2ff9afb4a0d95105509a384bad0ff8e8c3f828f
Ayomikun Orogun <ayomikunorogun@gmail.com>
2024-12-06 14:16:34 -0600

    Add CORS support to Lambda function

--- lib/lambda/lambda_function.py
+++ lib/lambda/lambda_function.py
@@ -8,11 +8,11 @@ import validators
 from datetime import datetime, timezone, timedelta
 
 logging.basicConfig(
-    format='{levelname} {asctime} {message}',
-    style='{',
-    datefmt='%Y-%m-%d %H:%M:%S', 
+    format="{levelname} {asctime} {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.DEBUG,
-    encoding='utf-8'
+    encoding="utf-8",
 )
 
 ddb_client = boto3.client("dynamodb")
@@ -20,88 +20,116 @@ URL_SHORTENER_DDB_TABLE = os.environ["URL_SHORTENER_DDB_TABLE"]
 
 
 def lambda_handler(event, context):
-    timeToLiveInSeconds = 259200 # Three days, it could be configurable in future
+    timeToLiveInSeconds = 259200  # Three days, it could be configurable in future
+    cors_headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Credentials": "true",
+    }
     response = {
         "statusCode": 200,
-        "body": json.dumps({ 
-            "message": "Welcome to my URL shortening service!"
-        })
+        "headers": cors_headers,
+        "body": json.dumps({"message": "Welcome to my URL shortening service!"}),
     }
 
-    if event["httpMethod"] == "POST" and event['body']:
-        payload = json.loads(event['body'])
-        originalUrl = payload['originalUrl']
+    if event["httpMethod"] == "OPTIONS":
+        response.update(
+            {"body": json.dumps({"message": "Preflight Request Successful"})}
+        )
+
+    if event["httpMethod"] == "POST" and event["body"]:
+        payload = json.loads(event["body"])
+        originalUrl = payload["originalUrl"]
 
         if event["path"] == "/" and is_valid_url(originalUrl):
-            short_url_id = create_short_url(originalUrl) 
+            short_url_id = create_short_url(originalUrl)
             ddb_dict_item = {}
 
             try:
                 ddb_dict_item = get_short_url_data(short_url_id)
             except Exception as e:
-                response['body'] = json.dumps({ 
-                        "error":"Unable to create short URL, please try again" 
-                    })
-                logging.info(f'An error occurred: {e}')
-                logging.error('Failed to retrieve Short URL data', exc_info=True)
-            
+                response["body"] = json.dumps(
+                    {"error": "Unable to create short URL, please try again"}
+                )
+                logging.info(f"An error occurred: {e}")
+                logging.error("Failed to retrieve Short URL data", exc_info=True)
+
             if ddb_dict_item and not is_short_url_expired(ddb_dict_item):
-                response.update({
-                        "body": json.dumps({
-                            "shortUrl": ddb_dict_item['shortUrlId']
-                        })
-                    })
+                response.update(
+                    {"body": json.dumps({"shortUrl": ddb_dict_item["shortUrlId"]})}
+                )
             else:
                 try:
-                    ddb_update_response = add_short_url_to_database(short_url_id, originalUrl, timeToLiveInSeconds)
-                    
-                    response.update({
-                        "statusCode": ddb_update_response,
-                        "body": json.dumps({
-                            "shortUrl": short_url_id
-                        })
-                    })
+                    ddb_update_response = add_short_url_to_database(
+                        short_url_id, originalUrl, timeToLiveInSeconds
+                    )
+
+                    response.update(
+                        {
+                            "statusCode": ddb_update_response,
+                            "body": json.dumps({"shortUrl": short_url_id}),
+                        }
+                    )
                 except Exception as e:
-                    response.update({
-                        "body": json.dumps({
-                            "error":"Unable to create short URL, please try again" 
-                        })
-                    })
+                    response.update(
+                        {
+                            "body": json.dumps(
+                                {
+                                    "error": "Unable to create short URL, please try again"
+                                }
+                            )
+                        }
+                    )
                     logging.error("Failed to add Short URL to database", exc_info=True)
         else:
-            response.update({
-                "body": json.dumps({
-                    "error": "Invalid URL. Please send a valid URL to be shortened" 
-                })
-            })
-    
-    elif event["httpMethod"] == 'GET':
+            response.update(
+                {
+                    "body": json.dumps(
+                        {
+                            "error": "Invalid URL. Please send a valid URL to be shortened"
+                        }
+                    )
+                }
+            )
+
+    elif event["httpMethod"] == "GET":
         # Extract short URL ID from the path
-        short_url_id = event.get('path')[1:]
+        short_url_id = event.get("path")[1:]
 
         try:
             response_dict = redirect_to_original_url(short_url_id)
             response.update(response_dict)
         except Exception as e:
-            response.update({
-                "body": json.dumps({
-                    "error": "Something went wrong, unable to redirect to destination" 
-                })
-            })
-            logging.info(f'An error occurred: {e}')
+            response.update(
+                {
+                    "body": json.dumps(
+                        {
+                            "error": "Something went wrong, unable to redirect to destination"
+                        }
+                    )
+                }
+            )
+            logging.info(f"An error occurred: {e}")
             logging.error("Failed to redirect to destination", exc_info=True)
-    
+
     else:
-        response.update({
-            "body": json.dumps({
-                "error": "Invalid request. Please review the request and try again" 
-            })
-        })
-    
+        response.update(
+            {
+                "body": json.dumps(
+                    {
+                        "error": "Invalid request. Please review the request and try again"
+                    }
+                )
+            }
+        )
+
     return response
+
 
 def is_valid_url(originalUrl: str) -> bool:
     return validators.url(originalUrl)
+
 
 def create_short_url(original_url: str) -> str:
     encoded_url = original_url.encode("utf-8")
@@ -109,6 +137,7 @@ def create_short_url(original_url: str) -> str:
     hash_object = hashlib.md5(encoded_url)
 
     return hash_object.hexdigest()
+
 
 def get_short_url_data(short_url_id: str) -> str:
     ddb_dict_item = None
@@ -118,69 +147,72 @@ def get_short_url_data(short_url_id: str) -> str:
         Key={
             "shortUrlId": {"S": short_url_id},
         },
-        ProjectionExpression='shortUrlId, originalUrl, timeToLiveInSeconds, createdAt',
+        ProjectionExpression="shortUrlId, originalUrl, timeToLiveInSeconds, createdAt",
     )
 
-    if ddb_response.get('Item'):
-        ddb_dict_item = dynamodb_item_to_dict(ddb_response.get('Item'))
+    if ddb_response.get("Item"):
+        ddb_dict_item = dynamodb_item_to_dict(ddb_response.get("Item"))
 
     return ddb_dict_item
 
-def add_short_url_to_database(short_url_id: str, originalUrl: str, timeToLiveInSeconds: int) -> int:
+
+def add_short_url_to_database(
+    short_url_id: str, originalUrl: str, timeToLiveInSeconds: int
+) -> int:
     ddb_response = ddb_client.put_item(
         TableName=URL_SHORTENER_DDB_TABLE,
         Item={
             "shortUrlId": {"S": short_url_id},
             "timeToLiveInSeconds": {"N": str(timeToLiveInSeconds)},
-            "createdAt": {"S": datetime.now(timezone.utc).isoformat()}, # ISO Format returns string
+            "createdAt": {
+                "S": datetime.now(timezone.utc).isoformat()
+            },  # ISO Format returns string
             "originalUrl": {"S": originalUrl},
         },
     )
 
     return ddb_response["ResponseMetadata"]["HTTPStatusCode"]
 
+
 def dynamodb_item_to_dict(dynamodb_item: dict) -> dict:
     if not dynamodb_item:
         raise Exception("No DynamoDB Item to convert")
-    
+
     deserializer = TypeDeserializer()
-    return {key: deserializer.deserialize(value) for key, value in dynamodb_item.items()}
-    
+    return {
+        key: deserializer.deserialize(value) for key, value in dynamodb_item.items()
+    }
+
+
 def is_short_url_expired(ddb_dict_item: dict) -> bool:
-    createdAt = datetime.fromisoformat(ddb_dict_item['createdAt'])
-    timeToLiveInSeconds = int(ddb_dict_item['timeToLiveInSeconds'])
-    return datetime.now(timezone.utc) > (createdAt + timedelta(seconds=timeToLiveInSeconds))
+    createdAt = datetime.fromisoformat(ddb_dict_item["createdAt"])
+    timeToLiveInSeconds = int(ddb_dict_item["timeToLiveInSeconds"])
+    return datetime.now(timezone.utc) > (
+        createdAt + timedelta(seconds=timeToLiveInSeconds)
+    )
+
 
 def redirect_to_original_url(short_url_id: str) -> dict:
     response_dict = {}
     ddb_dict_item = get_short_url_data(short_url_id)
-    
+
     if ddb_dict_item:
         if not is_short_url_expired(ddb_dict_item):
             response_dict = {
                 "statusCode": 301,
-                "headers":{
-                    "Location": ddb_dict_item['originalUrl'],
-                }
+                "headers": {
+                    "Location": ddb_dict_item["originalUrl"],
+                },
             }
         else:
             response_dict = {
                 "statusCode": 404,
-                "body": json.dumps({ 
-                    "message": "The specified short URL has expired"
-                })
+                "body": json.dumps({"message": "The specified short URL has expired"}),
             }
     else:
         response_dict = {
             "statusCode": 404,
-            "body": json.dumps({
-                "message": "Invalid short URL"
-            })
+            "body": json.dumps({"message": "Invalid short URL"}),
         }
-    
+
     return response_dict
-
-    
-
-    
-
--- lib/url-shortener-cdk-stack.ts
+++ lib/url-shortener-cdk-stack.ts
@@ -1,11 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as logs from 'aws-cdk-lib/aws-logs';
-
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as logs from "aws-cdk-lib/aws-logs";
 
 interface UrlShortenerCdkStackProps extends cdk.StackProps {
   urlShortenerFunctionLayerArn: string;
@@ -17,62 +16,95 @@ export class UrlShortenerCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // Import the DynamoDB table
-    const importedUrlShortenerDDBTable = dynamodb.TableV2.fromTableAttributes(this, 'ImportedUrlShortenerDDBTable', {
-      tableArn: props.urlShortenerDDBTableArn,
-    });
+    const importedUrlShortenerDDBTable = dynamodb.TableV2.fromTableAttributes(
+      this,
+      "ImportedUrlShortenerDDBTable",
+      {
+        tableArn: props.urlShortenerDDBTableArn,
+      }
+    );
 
     // Import Lambda layer
-    const importedUrlShortenerFunctionLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'ImportedUrlShortenerFunctionLayer', props.urlShortenerFunctionLayerArn);
+    const importedUrlShortenerFunctionLayer =
+      lambda.LayerVersion.fromLayerVersionArn(
+        this,
+        "ImportedUrlShortenerFunctionLayer",
+        props.urlShortenerFunctionLayerArn
+      );
 
     // LAMBDA RESOURCES
-    const urlShortenerFunctionRole = new iam.Role(this, 'UrlShortenerFunctionRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-    });
-
-    const urlShortenerFunction = new lambda.Function(this, 'UrlShortenerFunction', {
-      runtime: lambda.Runtime.PYTHON_3_13,
-      handler: 'lambda_function.lambda_handler',
-      code: lambda.Code.fromAsset('lib/lambda'),
-      role: urlShortenerFunctionRole,
-      layers: [importedUrlShortenerFunctionLayer],
-      environment: {
-        'URL_SHORTENER_DDB_TABLE': importedUrlShortenerDDBTable.tableArn
+    const urlShortenerFunctionRole = new iam.Role(
+      this,
+      "UrlShortenerFunctionRole",
+      {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
       }
-    });
+    );
 
-    urlShortenerFunctionRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
-    urlShortenerFunctionRole.addToPolicy(new iam.PolicyStatement({
-      actions: [
-        'dynamodb:PutItem',
-        'dynamodb:GetItem',
-        'dynamodb:Query',
-        'dynamodb:Scan',
-        'dynamodb:UpdateItem',
-      ],
-      resources: [importedUrlShortenerDDBTable.tableArn],
-    }));
+    const urlShortenerFunction = new lambda.Function(
+      this,
+      "UrlShortenerFunction",
+      {
+        runtime: lambda.Runtime.PYTHON_3_13,
+        handler: "lambda_function.lambda_handler",
+        code: lambda.Code.fromAsset("lib/lambda"),
+        role: urlShortenerFunctionRole,
+        layers: [importedUrlShortenerFunctionLayer],
+        environment: {
+          URL_SHORTENER_DDB_TABLE: importedUrlShortenerDDBTable.tableArn,
+        },
+      }
+    );
+
+    urlShortenerFunctionRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+    urlShortenerFunctionRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem",
+        ],
+        resources: [importedUrlShortenerDDBTable.tableArn],
+      })
+    );
 
     // API GATEWAY REST API RESOURCES
-    const UrlShortenerApiAccessLogsLogGroup = new logs.LogGroup(this, "UrlShortenerApiAccessLogs", {
-      retention: logs.RetentionDays.ONE_DAY,
-    });
-
-    const urlShortenerApi = new apigateway.LambdaRestApi(this, 'UrlShortenerApi', {
-      handler: urlShortenerFunction,
-      proxy: false,
-      cloudWatchRole: true,
-      cloudWatchRoleRemovalPolicy: cdk.RemovalPolicy.DESTROY,
-      deployOptions: {
-        accessLogDestination: new apigateway.LogGroupLogDestination(UrlShortenerApiAccessLogsLogGroup),
-        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields()
+    const UrlShortenerApiAccessLogsLogGroup = new logs.LogGroup(
+      this,
+      "UrlShortenerApiAccessLogs",
+      {
+        retention: logs.RetentionDays.ONE_DAY,
       }
-    });
+    );
+
+    const urlShortenerApi = new apigateway.LambdaRestApi(
+      this,
+      "UrlShortenerApi",
+      {
+        handler: urlShortenerFunction,
+        proxy: false,
+        cloudWatchRole: true,
+        cloudWatchRoleRemovalPolicy: cdk.RemovalPolicy.DESTROY,
+        deployOptions: {
+          accessLogDestination: new apigateway.LogGroupLogDestination(
+            UrlShortenerApiAccessLogsLogGroup
+          ),
+          accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields(),
+        },
+      }
+    );
 
     // User makes POST request, passing in original URL to be shortened
-    urlShortenerApi.root.addMethod('POST');
+    urlShortenerApi.root.addMethod("POST");
 
     // User makes GET /{shortUrlId} request to get redirected to original destination
-    const shortUrlIdResource = urlShortenerApi.root.addResource('{shortUrlId}');
-    shortUrlIdResource.addMethod('GET');
+    const shortUrlIdResource = urlShortenerApi.root.addResource("{shortUrlId}");
+    shortUrlIdResource.addMethod("GET");
   }
 }
