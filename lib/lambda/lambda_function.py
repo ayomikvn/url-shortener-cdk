6c4f99d0885bcba203235b5d7e93ea343b0d60d5
Ayomikun Orogun <ayomikunorogun@gmail.com>
2025-03-17 09:20:14 -0500

    Update Lambda function code with return statements

--- lib/lambda/lambda_function.py
+++ lib/lambda/lambda_function.py
@@ -6,18 +6,18 @@ url_shortener = UrlShortener()
 
 def lambda_handler(event, context):
     if event["httpMethod"] == "OPTIONS":
-        url_shortener.handle_options()
+        return url_shortener.handle_options()
     
     elif event["httpMethod"] == "GET":
         short_url_id = event.get("path")[1:]
-        url_shortener.handle_get(short_url_id)
+        return url_shortener.handle_get(short_url_id)
 
     elif event["httpMethod"] == "POST" and event["body"]:
         payload = json.loads(event["body"])
         original_url = payload.get("originalUrl", "")
         request_path = event["path"]
 
-        url_shortener.handle_post(request_path, original_url)
+        return url_shortener.handle_post(request_path, original_url)
         
     else:
         url_shortener.set_client_side_error("Invalid request. Please review the request and try again")
